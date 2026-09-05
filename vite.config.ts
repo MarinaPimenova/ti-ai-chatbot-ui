02f06534521966d072a98a2039b8e4bf98b3c797
Marina Pimenova <mnfpma@gmail.com>
2026-09-05 21:57:33 +0300

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import {ConfigEnv, defineConfig, mergeConfig} from 'vitest/config';
+import viteConfig from './vite.config';
+import {UserConfig} from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(async (configEnv) => {
+  // If vite.config.ts exports a function, call it to get the actual UserConfig object
+  const resolvedViteConfig = typeof viteConfig === 'function'
+      ? await (viteConfig as (env: ConfigEnv) => UserConfig | Promise<UserConfig>)(configEnv)
+      : viteConfig;
+
+  return mergeConfig(
+      resolvedViteConfig,
+      defineConfig({
+        test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: ['./vitest.setup.ts'],
+          coverage: {
+            reporter: ['html', 'lcovonly', 'text-summary'],
+          },
+        },
+        css: {
+          preprocessorOptions: {
+            scss: {
+              silenceDeprecations: ['legacy-js-api'],
+            },
+          },
+          modules: {
+            scopeBehaviour: 'local',
+            generateScopedName: (name) => `${name}`,
+          },
+        },
+      })
+  );
+});
